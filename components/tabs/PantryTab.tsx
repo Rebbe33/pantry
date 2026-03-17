3de3210b89f8099abaf78b26069f7f8da6f47b5b
Rebbe33 <gaboriaud.r@gmail.com>
2026-03-17 18:18:10 +0100

    Update PantryTab.tsx

--- components/tabs/PantryTab.tsx
+++ components/tabs/PantryTab.tsx
@@ -393,7 +393,6 @@ export default function PantryTab() {
                           </button>
                         </div>
                       </div>
-                      </div>
                     </motion.div>
                   )
                 })}
